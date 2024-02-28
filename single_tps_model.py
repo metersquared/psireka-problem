eedab56474becc302d1af121362fd13e55e838b1
Arya Prasetya <arya.prasetya@tu-berlin.de>
2024-02-28 22:32:43 +0100

    single tps model

--- single_tps_model.py
+++ single_tps_model.py
@@ -2,19 +2,62 @@ import numpy as np
 
 
 def to_array(num: int, b=10):
+    """Convert number to array of digits.
+
+    Parameters
+    ----------
+    num : int
+        Number
+    b : int, optional
+        Base, by default 10
+
+    Returns
+    -------
+    ndarray
+        Array of digits
+    """
     n = np.ceil(np.max(np.log(num) / np.log(b))).astype(int)
     d = np.arange(n)
     return num // b**d % b
 
 
 def to_integer(array: np.ndarray, b=10):
+    """Convert array of digits to number.
+
+    Parameters
+    ----------
+    array : np.ndarray
+        Array of digits.
+    b : int, optional
+        Base, by default 10
+
+    Returns
+    -------
+    int
+        Number
+    """
     num = 0
     for n, digit in enumerate(array):
         num = num + digit * b**n
     return num
 
 
-def approach(target: int, init: int):
+def optimize(target: int, init: int):
+    """Approach a target number with only a single digit append or prepend.
+
+    Parameters
+    ----------
+    target : int
+        Target number
+    init : int
+        Initial number
+
+    Returns
+    -------
+    int
+        Optimal number
+    """
+
     res = target - init
 
     if res == 0:
@@ -59,7 +102,7 @@ rng = np.random.default_rng(58)
 
 candidates = 3  # Number of candidate
 ideal_dist = [0.24, 0.58, 0.18]  # Number of distribution that we would ideally want
-max_vote = 200  # Maximum number of votes that can be given to a candidate
+max_vote = 1000  # Maximum number of votes that can be given to a candidate
 
 votes = rng.integers(
     max_vote, size=(candidates,)
@@ -68,19 +111,17 @@ total_vote = np.sum(votes)
 
 votes_dist = votes / total_vote  # Distribution of votes
 ideal_dist = np.array(ideal_dist / np.sum(ideal_dist))  # Ideal distribution of votes
+ideal_votes = np.rint(ideal_dist * total_vote)
 
-ideal_num = rng.integers(200)
-actual_num = rng.integers(50)
+manipulated_votes = np.zeros(candidates)
 
-print(ideal_num)
-print(actual_num)
+for idx, vote in enumerate(votes):
+    manipulated_votes[idx] = optimize(ideal_votes[idx], vote)
 
-array1 = to_array(ideal_num)
-array2 = to_array(actual_num)
-print(array1)
-print(array2)
+print("Actual vote :", votes)
+print("Ideal vote :", ideal_votes)
+print("Manipulated vote :", manipulated_votes)
 
-print(to_integer(array1))
-print(to_integer(array2))
-
-print(approach(15, 47))
+print("Actual vote distribution :", votes_dist)
+print("Ideal vote distribution :", ideal_dist)
+print("Manipulated vote distribution :", manipulated_votes / sum(manipulated_votes))
